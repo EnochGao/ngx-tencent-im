69b74f26f6d04ccb6eb71ab0d6d12ace352cc77f
enochgao <enochgao@qq.com>
2021-05-25 10:37:01 +0800

    chore: update

--- projects/ng-tencent-im/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng-tencent-im
- */
-
-export * from './lib/ng-tencent-im.service';
-export * from './lib/ng-tencent-im.component';
-export * from './lib/ng-tencent-im.module';
