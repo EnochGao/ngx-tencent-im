9d68f83ffa2ab81772000792b302a8df1721dfc2
enochgao <enochgao@qq.com>
2021-07-02 15:32:09 +0800

    chore: chore

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/winkongx-tencent-im'),
+      dir: require('path').join(__dirname, './coverage/ngx-tencent-im-demo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
